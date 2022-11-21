0e637f6019a2a0f205adf51d48b862d9dc482c13
Charles Tyra <charlestyra@macbook-pro.myfiosgateway.com>
2022-11-21 16:20:57 -0500

    Monkey Hashing #my_zip method, Charles driving and Zahi navigating.

--- Enumerables.rb
+++ Enumerables.rb
@@ -52,7 +52,17 @@ class Array
         flatten
     end
 
-    def my_zip
+    def my_zip(*args)
+        my_arr = Array.new(self.length) { Array.new(args.length + 1) }
+        temp_arr = [self] + args
+        
+            temp_arr.each_with_index do |arr, i|
+                if i < my_arr[0].length 
+                    arr.each_with_index { |ele, j| my_arr[j][i] = ele if j < my_arr.length }
+                end 
+            end
+        my_arr
+    end
 end
 
 # calls my_each twice on the array, printing all the numbers twice.
@@ -91,3 +101,12 @@ p "flatten"
 p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
 
 p "zip"
+a = [ 4, 5, 6 ]
+b = [ 7, 8, 9 ]
+p [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+p a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
+p [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
+
+c = [10, 11, 12]
+d = [13, 14, 15]
+p [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
