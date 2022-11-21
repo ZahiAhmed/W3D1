3073bf0afd042f83542a9f177d605c79f8d23439
Charles Tyra <charlestyra@macbook-pro.myfiosgateway.com>
2022-11-21 15:19:10 -0500

    Contains paired programming of the #my_reject, #my_any?, and #my_all? methods - Zahi Navigating, Charles driving

--- Enumerables.rb
+++ Enumerables.rb
@@ -15,24 +15,59 @@ class Array
         end
         select_array
     end
+
+    def my_reject(&blk)
+        reject_array = []
+
+        self.my_each do |ele|
+            reject_array << ele if !blk.call(ele)
+        end
+        reject_array
+    end
+
+    def my_any?(&blk)
+        self.my_each do |ele|
+            return true if blk.call(ele)
+        end
+        false
+    end
+
+    def my_all?(&blk)
+        self.my_each do |ele|
+            return false if !blk.call(ele)
+        end
+        true
+    end
 end
 
 # calls my_each twice on the array, printing all the numbers twice.
 return_value = [1, 2, 3].my_each do |num|
-    puts num
-   end.my_each do |num|
-    puts num
-   end
-   # => 1
-       2
-       3
-       1
-       2
-       3
-   
-   p return_value  # => [1, 2, 3]
+puts num
+end.my_each do |num|
+puts num
+end
+# => 1
+    2
+    3
+    1
+    2
+    3
 
-   p "select"
-   a = [1, 2, 3]
-   p a.my_select { |num| num > 1 } # => [2, 3]
-   p a.my_select { |num| num == 4 } # => []
+p return_value  # => [1, 2, 3]
+
+p "select"
+a = [1, 2, 3]
+p a.my_select { |num| num > 1 } # => [2, 3]
+p a.my_select { |num| num == 4 } # => []
+
+p "reject"
+a = [1, 2, 3]
+p a.my_reject { |num| num > 1 } # => [1]
+p a.my_reject { |num| num == 4 } # => [1, 2, 3]
+
+p "any?"
+a = [1, 2, 3]
+p a.my_any? { |num| num > 1 } # => true
+p a.my_any? { |num| num == 4 } # => false
+p a.my_all? { |num| num > 1 } # => false
+p a.my_all? { |num| num < 4 } # => true
